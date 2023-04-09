885a8c7a341b26160a8040127cf58793e8eccad4
Te3Ka_PaynE <Mnement4813@yandex.ru>
2023-04-09 18:06:34 +0300

    1 задание. Выполнено

--- Exercise01/main.py
+++ Exercise01/main.py
@@ -14,3 +14,26 @@ def author():
     print('79811131773@yandex.ru')
     print('****************************')
 
+# Проверка на введение трёхзначного числа.
+def correct_number():
+    _number = int(input("Введите трёхзначное число: "))
+    if (_number < 0):
+        _number = _number * (-1)
+    while (_number < 100) or (_number > 999):
+        _number = int(input("Введено неверное число! Введите заново: "))
+    return _number
+
+# Сложение цифр числа.
+def sum_digits(number):
+    sum = 0
+    while (number > 0):
+        sum += number % 10
+        number //= 10
+    return sum
+
+print("Программа считает сумму цифр трёхзначного числа.")
+_num = correct_number()
+_sum_dig = sum_digits(_num)
+print(f"Сумма цифр числа {_num} = {_sum_dig}")
+
+author()
