b79ee64d5298c18484448696c11f944602fe9d92
Te3Ka_PaynE <Mnement4813@yandex.ru>
2023-04-09 18:58:45 +0300

    4 подправлено

--- Exercise04/main.py
+++ Exercise04/main.py
@@ -9,10 +9,33 @@
 (то есть разломить шоколадку на два прямоугольника)
 '''
 
-# Функция вывода автора программы
+# Функция вывода автора программы.
 def author():
     print('****************************')
     print('Программа создана:')
     print('Илья "Te3K@_PaynE" Новичихин')
     print('79811131773@yandex.ru')
     print('****************************')
+
+# Проверка правильности введённого числа.
+def correct_number(str):
+    _number = int(input(f"Введите {str}: "))
+    while (_number < 0):
+        _number = int(input("Введено неверное число! Введите заново: "))
+    return _number
+
+# Проверка на перелом шоколадки по количеству долек.
+def breacking_chocolate(_length_chocolate, _width_chocolate, _number_slices):
+    if (_number_slices % _length_chocolate == 0) or (_number_slices % _width_chocolate == 0) and (_number_slices <= _width_chocolate * _length_chocolate): 
+        return bool(True)
+    else:
+        return bool(False)
+
+print("Программа проверяет, можно ли за один разлом шоколадки отломить указанное количество долек.")
+_n = correct_number("длину шоколадки в дольках")
+_m = correct_number("ширину шоколадки в дольках")
+_k = correct_number("сколько долек нужно отломить")
+_res = breacking_chocolate(_n, _m, _k)
+print(f"От шоколадки размером {_n}*{_m} {'можно' if _res else 'нельзя'} отломить {_k} кусочков")
+
+author()
